b23a549f3e5d9e5c894b856a040bde472dc4171d
itsMyrto <75997814+itsMyrto@users.noreply.github.com>
2023-12-29 15:40:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from keras import Sequential
+from keras.src.engine.input_layer import InputLayer
+from keras.src.layers import Dense
+from keras.src.optimizers import Adam
 from scipy.spatial.distance import squareform, pdist
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
@@ -14,21 +18,28 @@ df = df.dropna()
 # Transforming the pandas dataframe to a numpy array in order to run the machine learning algorithms
 X_train = df.to_numpy()
 
+# Adding better precision in the pandas dataframe to save the normalized values
+pd.set_option('display.float_format', '{:.8f}'.format)
+
 # Normalize data using the MinMax between [0,1]
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 
+# Saving the transformed data back to the df
+df['x'] = X_train[:, 0]
+df['y'] = X_train[:, 1]
+
 # Running the kmeans algorithm using k>>5
-k = 300
-kmeans = KMeans(n_clusters=k, random_state=42)
+k = 100
+kmeans = KMeans(n_clusters=k, random_state=42, n_init="auto")
 
 # Applying the kmeans algorithm and then save for every point the cluster id it belongs in a new column in the dataframe
 df['cluster'] = kmeans.fit_predict(X_train)
 
 # Plotting the initial clusters with k>>5
-for cluster_id_initial in range(k):
-    cluster_data_initial = X_train[df['cluster'] == cluster_id_initial]
-    plt.scatter(cluster_data_initial[:, 0], cluster_data_initial[:, 1], label=f'Cluster {cluster_id_initial}', alpha=0.5)
+for cluster_id in range(k):
+    cluster_data = X_train[df['cluster'] == cluster_id]
+    plt.scatter(cluster_data[:, 0], cluster_data[:, 1], label=f'Cluster {cluster_id}', alpha=0.5)
 
 plt.title('Scatter Plot')
 plt.xlabel('x')
@@ -96,16 +107,87 @@ plt.ylabel('y')
 plt.show()
 
 
-
-
 # APPLY DBSCAN TO EACH CLUSTER AND DETECT THE OUTLIERS
 
 # This for loop iterates through all clusters, i ve made it for you so you don't have to understand my code
-# Here, the cluster variable in every loop contains a pandas dataframe for each cluster. 
+# Here, the cluster variable in every loop contains a pandas dataframe for each cluster.
 # You have to perform dbscan in every cluster and save the outliers. Then, print the outliers and make a cute plot
 # if you need something from the df dataframe it contains three columns:
 # one for x, one for y and one called 'cluster' that contains the cluster id.
 
+total = 0
 for cluster_id in np.unique(df['cluster']):
     cluster = df[df['cluster'] == cluster_id][['x', 'y']]
-    # Perform DBSCAN
+    print(cluster)
+    total = len(cluster) + total
+print(total)
+
+
+
+
+
+
+# ΝΕΥΡΩΝΙΚΟ ΔΙΚΤΥΟ
+
+#
+# outliers = []
+#
+# for cluster_id_final in range(5):
+#
+#     cluster_data_final = X_train[df['cluster'] == final_clusters[cluster_id_final]]
+#
+#     # Train autoencoder
+#     OPTIMIZER_1 = Adam(learning_rate=0.001)
+#     autoencoder = Sequential()
+#     autoencoder.add(InputLayer(input_shape=(2,)))
+#     autoencoder.add(Dense(units=2, activation="linear", use_bias=True))
+#     autoencoder.add(Dense(units=3, activation="linear", use_bias=True))
+#     autoencoder.add(Dense(units=2, activation="linear", use_bias=True))
+#     autoencoder.add(Dense(units=1, activation="linear", use_bias=True))
+#     autoencoder.add(Dense(units=2, activation="linear", use_bias=True))
+#     autoencoder.add(Dense(units=3, activation="linear", use_bias=True))
+#     autoencoder.add(Dense(units=2, activation="linear", use_bias=True))
+#     autoencoder.compile(optimizer=OPTIMIZER_1, loss='mean_squared_error')
+#     autoencoder.fit(cluster_data_final, cluster_data_final, epochs=25, batch_size=32, shuffle=True)
+#
+#     # Predict and calculate reconstruction errors
+#     reconstructed_data = autoencoder.predict(cluster_data_final)
+#     reconstruction_errors = np.mean(np.square(cluster_data_final - reconstructed_data), axis=1)
+#
+#     # Set a threshold for outliers within the cluster
+#     threshold = np.percentile(reconstruction_errors, 99.9)
+#
+#     # Identify outliers within the cluster
+#     cluster_outliers = cluster_data_final[reconstruction_errors > threshold]
+#     outliers.append(cluster_outliers)
+#
+# # Concatenate outliers from all clusters
+# all_outliers = np.concatenate(outliers, axis=0)
+#
+#
+# # Plot original data and connected clusters
+# for cluster_id_final in range(5):
+#     cluster_data_final = df[df['cluster'] == final_clusters[cluster_id_final]]
+#     plt.scatter(
+#         cluster_data_final['x'],
+#         cluster_data_final['y'],
+#         label=f'Final Cluster {cluster_id_final}',
+#         alpha=0.5,
+#         c=preferable_colors[cluster_id_final % len(preferable_colors)]
+#     )
+#
+# all_outliers = np.concatenate(outliers)
+#
+# # Plot outliers
+# plt.scatter(
+#     all_outliers[:, 0],
+#     all_outliers[:, 1],
+#     label='Outliers',
+#     color='black',
+#     marker='x'
+# )
+#
+# plt.title('Outlier Detection with Autoencoders')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
